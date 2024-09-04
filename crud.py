2f37d567961b86462345cd25f0f1075c5782208f
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-09-04 11:32:42 +0300

    Added type annotation

--- crud.py
+++ crud.py
@@ -1,14 +1,16 @@
+from typing import List, Optional
+
 from sqlalchemy.orm import Session
 
 from models import Author, Book
 import schemas
 
 
-def get_all_authors(db: Session, skip: int = 0, limit: int = 10):
+def get_all_authors(db: Session, skip: int = 0, limit: int = 10) -> List[Author]:
     return db.query(Author).offset(skip).limit(limit).all()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> Author:
     db_author = Author(
         name=author.name,
         bio=author.bio
@@ -20,7 +22,7 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_author_by_id(db: Session, author_id: int):
+def get_author_by_id(db: Session, author_id: int) -> Optional[Author]:
     return db.query(Author).filter(Author.id == author_id).first()
 
 
@@ -29,14 +31,14 @@ def get_all_books(
         author_id: int = None,
         skip: int = 0,
         limit: int = 10
-):
+) -> List[Book]:
     query = db.query(Book)
     if author_id:
         query = query.filter(Author.id == author_id)
     return query.offset(skip).limit(limit).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> Book:
     db_book = Book(
         title=book.title,
         author_id=book.author_id,
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
@@ -19,7 +19,7 @@ def get_db() -> Session:
 
 
 @app.get("/")
-def read_root():
+def read_root() -> Dict[str, str]:
     return {"Hello": "World"}
 
 
@@ -28,17 +28,17 @@ def get_authors(
         skip: int | None = 0,
         limit: int | None = 10,
         db: Session = Depends(get_db)
-):
+) -> List[schemas.Author]:
     return crud.get_all_authors(db=db, skip=skip, limit=limit)
 
 
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
-def get_author_by_id(author_id: int, db: Session = Depends(get_db)):
+def get_author_by_id(author_id: int, db: Session = Depends(get_db)) -> schemas.Author:
     return crud.get_author_by_id(db=db, author_id=author_id)
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)) -> schemas.Author:
     return crud.create_author(db=db, author=author)
 
 
@@ -47,12 +47,12 @@ def get_books(
         skip: int = 0,
         limit: int = 10,
         db: Session = Depends(get_db)
-):
+) -> List[schemas.Book]:
     return crud.get_all_books(db=db, skip=skip, limit=limit)
 
 
 @app.get("/books/{author_id}/", response_model=list[schemas.Book])
-def get_book_by_author_id(author_id: int, db: Session = Depends(get_db)):
+def get_book_by_author_id(author_id: int, db: Session = Depends(get_db)) -> List[schemas.Book]:
     return crud.get_all_books(author_id=author_id, db=db)
 
 
@@ -60,5 +60,5 @@ def get_book_by_author_id(author_id: int, db: Session = Depends(get_db)):
 def create_book(
         book: schemas.BookCreate,
         db: Session = Depends(get_db)
-):
+) -> schemas.Book:
     return crud.create_book(db=db, book=book)
